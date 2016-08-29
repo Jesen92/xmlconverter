c32e5a4cbee3eb5daefca050114b168427cebc1a
Jesen92 <hjesenovic@gmail.com>
2016-08-29 17:50:40 +0200

    Kreirano generiranje izvjesca, napravljen rescue u slucaju greske kod generiranja izvjesca

--- app/controllers/generate_pdf_controller.rb
+++ app/controllers/generate_pdf_controller.rb
@@ -4,17 +4,20 @@ class GeneratePdfController < ApplicationController
   def show
     @obrazac = Zaglavlje.find(params[:id])
 
-    @iznos_racun = 0
-    @iznos_pdv = 0
-    @ukupan_iznos_racuna_s_pdv = 0
-    @placeni_iznos_racuna = 0
-    @neplaceni_dio_racuna = 0
+    @iznos_racuna = 0.00
+    @iznos_pdv = 0.00
+    @ukupan_iznos_racuna_s_pdv = 0.00
+    @placeni_iznos_racuna = 0.00
+    @neplaceni_dio_racuna = 0.00
 
-    @ukupno_iznos_racun = 0
-    @ukupno_iznos_pdv = 0
-    @ukupno_ukupan_iznos_racuna_s_pdv = 0
-    @ukupno_placeni_iznos_racuna = 0
-    @ukupno_neplaceni_dio_racuna = 0
+    @ukupno_iznos_racuna = 0.00
+    @ukupno_iznos_pdv = 0.00
+    @ukupno_ukupan_iznos_racuna_s_pdv = 0.00
+    @ukupno_placeni_iznos_racuna = 0.00
+    @ukupno_neplaceni_dio_racuna = 0.00
+
+    @rb_kupca = 1
+    @rb_racuna = 1
 
     respond_to do |format|
         format.html
@@ -22,7 +25,11 @@ class GeneratePdfController < ApplicationController
           render pdf: "file_name" , :template => 'generate_pdf/show.html.erb',  # Excluding ".pdf" extension.
                  orientation: 'Landscape'
         end
-      end
+    end
+
+  rescue => e
+    flash[:alert] = "Pogreška kod kreiranja izvješća! Provjerite da li su sva polja u obrascu pravilno ispunjena!\nError message: #{e.message}"
+    redirect_to(:back)
   end
 
 end
